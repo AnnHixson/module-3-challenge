428edf654aaed0a5d1150a7d351b93d3e76d804c
Ann <ann.hixson@uconn.edu>
2022-09-29 21:37:05 -0400

    javascript updated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # module-3-challenge
+
+
+credit
+Accessing elements in nested arrays: [https://www.codecademy.com/forum_questions/54a31674d3292ff5640169ca](https://www.codecademy.com/forum_questions/54a31674d3292ff5640169ca)
--- script.js
+++ script.js
@@ -9,12 +9,43 @@ function writePassword() {
   passwordText.value = password;
 
 
-  // Prompts
-  // window.alert("Please answer the following questions in order to set the parameters for your password.");
+
+  // testing
+  
+
+
+  // Beginning of my code
+
+  var numberOfCharacters;
+  var lowerCase;
+  var upperCase;
+  var numeric;
+  var special;
+
+  var characterCount;
+
+  var optionsLowerCase;
+  var optionsUpperCase;
+  var optionsNumeric;
+  var optionsSpecial;
+  var optionsOptions;
+
+  var selectArray;
+  var character;
+
+
+  // testing b
+  // var actualPassword = [];
+
+
+
+
+  // Begin Prompts
+  window.alert("Please answer the following questions in order to set the parameters for your password.");
 
   // Select number of characters
   function setNumber() {
-    var numberOfCharacters = prompt("Number of characters in password (from 8-128):");
+    numberOfCharacters = prompt("Number of characters in password (from 8-128):");
     // Invalid answers:
     //  Nothing entered
     if (numberOfCharacters === "") {
@@ -22,53 +53,129 @@ function writePassword() {
       setNumber();
     // Too small
     } else if (numberOfCharacters < 8) {
-      var tryAgainBigger = confirm("Number must be at least 8.");
+      alert("Number must be at least 8.");
       setNumber();
     // Too big
     } else if (numberOfCharacters > 128) {
-      var tryAgainSmaller = confirm("Number must be no more than 128.");
+      alert("Number must be no more than 128.");
       setNumber();
+    } else {
+      characterCount = numberOfCharacters;
     };
   };
   // Call function
-  // setNumber();
+  setNumber();
   
-
-  // Pasword Includes ...
+  // Pasword criteria selection
   function passwordIncludes() {
-    var lowerCase = confirm("Include lowercase characters?");
-    var upperCase = confirm("Include uppercase characters?");
-    var numeric = confirm("Include numeric characters?");
-    var special = confirm("Include special characters?");
+    lowerCase = confirm("Include lowercase characters?");
+    upperCase = confirm("Include uppercase characters?");
+    numeric = confirm("Include numeric characters?");
+    special = confirm("Include special characters?");
     // no characters
     if (!lowerCase && !upperCase && !numeric && !special) {
-      var tryAgainPickOne = confirm("At least one type of character must be included.");
+      var tryAgainPickOne = alert("At least one type of character must be included.");
       passwordIncludes();
     };
   };
   // Call function
-  // passwordIncludes();
+  passwordIncludes();
 
-  // alert("Thank you.\nNow generating your password...");
+  // Conclude Prompts
+  alert("Thank you.\nNow generating your password...");
+
 
   // Generate password
   function generatePassword() {
     // character arrays
-    var optionsLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var optionsUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var optionsNumeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    var optionsSpecial = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+    optionsLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    optionsUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    optionsNumeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    optionsSpecial = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
     // array of character arrays
-    var optionsOptions = [optionsLowerCase, optionsUpperCase, optionsNumeric, optionsSpecial];
+    optionsOptions = [optionsLowerCase, optionsUpperCase, optionsNumeric, optionsSpecial];
 
-    // select an array
-    var selectArray = Math.floor(Math.random() * optionsOptions.length);
-    // select a character
-    var character = Math.floor(Math.random() * optionsOptions[selectArray].length);
+    // Which character arrays to use
+    // all yes
+    if (lowerCase && upperCase && numeric && special) {
+      optionsOptions = [optionsLowerCase, optionsUpperCase, optionsNumeric, optionsSpecial];
+      // one no
+    } else if (lowerCase && upperCase && numeric && !special) {
+      optionsOptions = [optionsLowerCase, optionsUpperCase, optionsNumeric];
+    } else if (lowerCase && upperCase && !numeric && special) {
+      optionsOptions = [optionsLowerCase, optionsUpperCase, optionsSpecial];
+    } else if (lowerCase && !upperCase && numeric && special) {
+      optionsOptions = [optionsLowerCase, optionsNumeric, optionsSpecial];
+    } else if (!lowerCase && upperCase && numeric && special) {
+      optionsOptions = [optionsUpperCase, optionsNumeric, optionsSpecial];
+      // two no's
+    } else if (lowerCase && upperCase && !numeric && !special) {
+      optionsOptions = [optionsLowerCase, optionsUpperCase];
+    } else if (lowerCase && !upperCase && numeric && !special) {
+      optionsOptions = [optionsLowerCase, optionsNumeric];
+    } else if (!lowerCase && upperCase && numeric && !special) {
+      optionsOptions = [optionsUpperCase, optionsNumeric];
+    } else if (lowerCase && !upperCase && !numeric && special) {
+      optionsOptions = [optionsLowerCase, optionsSpecial];
+    } else if (!lowerCase && upperCase && !numeric && special) {
+      optionsOptions = [optionsUpperCase, optionsSpecial];
+    } else if (!lowerCase && !upperCase && numeric && special) {
+      optionsOptions = [optionsNumeric, optionsSpecial];
+      // three no's
+    } else if (lowerCase && !upperCase && !numeric && !special) {
+      optionsOptions = [optionsLowerCase];
+    } else if (!lowerCase && upperCase && !numeric && !special) {
+      optionsOptions = [optionsUpperCase];
+    } else if (!lowerCase && !upperCase && numeric && !special) {
+      optionsOptions = [optionsNumeric];
+    } else if (!lowerCase && !upperCase && !numeric && special) {
+      optionsOptions = [optionsSpecial];
+    };
 
-    // testing
-    console.log(optionsOptions[selectArray][character]);
+
+    // testing b
+    // function actualPassword() {
+
+
+
+
+    // Determines the characters
+    for (characterCount; characterCount > 0; characterCount--) {
+      // select an array
+      selectArray = Math.floor(Math.random() * optionsOptions.length);
+      // select a character
+      character = Math.floor(Math.random() * optionsOptions[selectArray].length);
+      console.log(optionsOptions[selectArray][character]);
+
+      // testing b
+      // password = optionsOptions[selectArray][character];
+    };
+
+
+    // }
+
+      // testing b
+      // password = actualPassword();
+
   };
+  // Call function
+  generatePassword();
+
+      // testing b
+      // password = actualPassword();
+  // password();
+  // testing b
+  // console.log(password());
+  // password = actualPassword
+
+
+
+
+
+  // End of my code
+
+
+
 }
 
 // Add event listener to generate button
